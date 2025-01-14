e6cffb5c43e8a2676c74d108a21475f7911eeb82
Yakovenko Oleksii <mr.mnogo@gmail.com>
2025-01-14 12:38:47 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "tailwindcss";
+
+export default defineConfig({
+  plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+});
