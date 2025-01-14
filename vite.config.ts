12db38ba953fc4e46e7af716ac49792ddb401dc5
Yakovenko Oleksii <mr.mnogo@gmail.com>
2025-01-14 12:41:57 +0300

    upd vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
 
 export default defineConfig({
   plugins: [vue()],
   css: {
     postcss: {
-      plugins: [tailwindcss],
+      plugins: [tailwindcss, autoprefixer],
     },
   },
   server: {
